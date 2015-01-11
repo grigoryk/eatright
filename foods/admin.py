b060ea6ff3f908c26122e79a65d59cb3b3cc7eb2
Grigory Kruglov <grigory.kruglov@gmail.com>
2015-01-10 18:03:10 -0800

    base admin class; day meal admin

--- foods/admin.py
+++ foods/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from foods.models import FoodServing, FoodCombo, FoodComboHasServing
+from foods.models import FoodServing, FoodCombo, FoodComboHasServing, DayMeals
 
 
-class FoodServingAdmin(admin.ModelAdmin):
-    list_display = (
-        'name', 'serving_name', 'calories', 'fat', 'carbs',
-        'fibre', 'protein', 'sodium')
+class BaseWithProperties(admin.ModelAdmin):
+    list_display = ('calories', 'fat', 'carbs', 'fibre', 'protein', 'sodium')
+
+
+class FoodServingAdmin(BaseWithProperties):
+    list_display = ('name', 'serving_name') + BaseWithProperties.list_display
 
 
 class FoodComboHasServingInline(admin.TabularInline):
@@ -13,14 +15,17 @@ class FoodComboHasServingInline(admin.TabularInline):
     extra = 2
 
 
-class FoodComboAdmin(admin.ModelAdmin):
-    list_display = (
-        'name', 'calories', 'fat', 'carbs',
-        'fibre', 'protein', 'sodium')
+class FoodComboAdmin(BaseWithProperties):
+    list_display = ('name',) + BaseWithProperties.list_display
 
     inlines = [FoodComboHasServingInline]
+
+
+class DayMealsAdmin(BaseWithProperties):
+    list_display = ('name',) + BaseWithProperties.list_display
 
 
 # Register your models here.
 admin.site.register(FoodServing, FoodServingAdmin)
 admin.site.register(FoodCombo, FoodComboAdmin)
+admin.site.register(DayMeals, DayMealsAdmin)
